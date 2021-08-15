63cedc25a9a2189783f94531b8900c77a774aab9
Baptistin Boilot <baptistin.boilot@epita.fr>
2021-08-15 21:24:45 +0200

    Merge PoC in java and play with ghidra extension system

--- src/main/java/ghidraautodetours/GhidraAutoDetoursAnalyzer.java
+++ src/main/java/ghidraautodetours/GhidraAutoDetoursAnalyzer.java
@@ -15,14 +15,32 @@
  */
 package ghidraautodetours;
 
+import java.io.IOException;
+
+import generic.continues.RethrowContinuesFactory;
+import ghidra.app.cmd.comments.SetCommentCmd;
 import ghidra.app.services.AbstractAnalyzer;
+import ghidra.app.services.AnalysisPriority;
 import ghidra.app.services.AnalyzerType;
+import ghidra.app.util.bin.MemoryByteProvider;
+import ghidra.app.util.bin.format.FactoryBundledWithBinaryReader;
+import ghidra.app.util.bin.format.mz.DOSHeader;
+import ghidra.app.util.bin.format.pe.Constants;
 import ghidra.app.util.importer.MessageLog;
+import ghidra.app.util.opinion.BinaryLoader;
+import ghidra.app.util.opinion.PeLoader;
 import ghidra.framework.options.Options;
+import ghidra.framework.store.LockException;
+import ghidra.program.model.address.Address;
+import ghidra.program.model.address.AddressOverflowException;
 import ghidra.program.model.address.AddressSetView;
+import ghidra.program.model.listing.CodeUnit;
+import ghidra.program.model.listing.Instruction;
 import ghidra.program.model.listing.Program;
 import ghidra.util.exception.CancelledException;
 import ghidra.util.task.TaskMonitor;
+import ghidraautodetours.GhidraAutoDetoursParser.Hook;
+import ghidraautodetours.GhidraAutoDetoursParser.MemorySpace;
 
 /**
  * TODO: Provide class-level documentation that describes what this analyzer does.
@@ -33,7 +51,8 @@ public class GhidraAutoDetoursAnalyzer extends AbstractAnalyzer {
 
 		// TODO: Name the analyzer and give it a description.
 
-		super("My Analyzer", "Analyzer description goes here", AnalyzerType.BYTE_ANALYZER);
+		super("AutoDetours", "Run the sample through AutoDetours and add the traces to the project.", AnalyzerType.BYTE_ANALYZER);
+		setPriority(AnalysisPriority.FUNCTION_ANALYSIS.after());
 	}
 
 	@Override
@@ -46,10 +65,28 @@ public class GhidraAutoDetoursAnalyzer extends AbstractAnalyzer {
 
 	@Override
 	public boolean canAnalyze(Program program) {
-
-		// TODO: Examine 'program' to determine of this analyzer should analyze it.  Return true
-		// if it can.
-
+		String format = program.getExecutableFormat();
+		if (format.equals(PeLoader.PE_NAME)) {
+			return true;
+		}
+		if (format.equals(BinaryLoader.BINARY_NAME)) {
+			MemoryByteProvider mbp = new MemoryByteProvider(program.getMemory(),
+				program.getAddressFactory().getDefaultAddressSpace());
+			try {
+				FactoryBundledWithBinaryReader reader = new FactoryBundledWithBinaryReader(
+					RethrowContinuesFactory.INSTANCE, mbp, true/*LittleEndian*/);
+				DOSHeader dosHeader = DOSHeader.createDOSHeader(reader);
+				if (dosHeader.e_magic() == DOSHeader.IMAGE_DOS_SIGNATURE) {
+					int peHeaderStartIndex = dosHeader.e_lfanew();
+					int peMagicNumber = reader.readInt(peHeaderStartIndex);
+					if (peMagicNumber == Constants.IMAGE_NT_SIGNATURE) {
+						return true;
+					}
+				}
+			}
+			catch (IOException e) {
+			}
+		}
 		return false;
 	}
 
@@ -58,8 +95,8 @@ public class GhidraAutoDetoursAnalyzer extends AbstractAnalyzer {
 
 		// TODO: If this analyzer has custom options, register them here
 
-		options.registerOption("Option name goes here", false, null,
-			"Option description goes here");
+		/*options.registerOption("Option name goes here", false, null,
+			"Option description goes here");*/
 	}
 
 	@Override
@@ -68,6 +105,37 @@ public class GhidraAutoDetoursAnalyzer extends AbstractAnalyzer {
 
 		// TODO: Perform analysis when things get added to the 'program'.  Return true if the
 		// analysis succeeded.
+		
+		String path = "/Users/baptistin/ghidra_scripts/ed01ebfbc9eb5bbea545af4d01bf5f1071661840480439c6e5babe8e080e41aa_Detours.jsonl";
+		GhidraAutoDetoursParser parser = new GhidraAutoDetoursParser(path);
+		try {
+			parser.parseJson();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+				
+		for (MemorySpace m : parser.getMemoryMap()) {
+			if (m.getFilename().equals("C:\\Temp\\sample.exe")) {
+				Address baseAddr = program.getAddressFactory().getDefaultAddressSpace().getAddress(m.getBeginAddr());
+				try {
+					program.setImageBase(baseAddr, true); // TODO : Add an option to the plugin
+				} catch (AddressOverflowException | LockException | IllegalStateException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		for (Hook h : parser.getHookResults()) {
+			Address addr = program.getAddressFactory().getDefaultAddressSpace().getAddress(h.getRetAddr());
+			Instruction inst = program.getListing().getInstructionBefore(addr);
+			
+			String comment = String.format("%s(%s) -> %s", h.getFncName(), String.join(", ", h.getFncArgs()), h.getFncRet());
+			SetCommentCmd cmd = new SetCommentCmd(inst.getAddress(), CodeUnit.EOL_COMMENT, comment);
+			cmd.applyTo(program);
+		}
 
 		return false;
 	}
